7cf6362a7e85fc2aa264961db7b9942e3ec6c7ee
wbravoanoni <wbravoanoni@gmail.com>
2018-01-25 19:41:38 -0300

    Primeros pasos JS

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//En Javascript
+
+var elemento =document.querySelector("#elementoJS").innerHTML="elementoJS Modificado";
+//var elemento =document.getElementById("elementoJS").innerHTML="elementoJS Modificado";
+
+//EN JQUERY
+
+var elemento=$("#elementoJQ").html("elementoJQ Modificado");
