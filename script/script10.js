1a0f94d5dac596400b3da6b23877ec856c310d50
wbravoanoni <wbravoanoni@gmail.com>
2018-01-27 23:24:35 -0300

    before after

---
+++ script/script10.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+console.log("DOM cargado script10");
+
+
+$(".contenido").find("p").css({"padding-bottom":"20px"});
+
+var enlaceI=$("<a href='#'>Link de inicio</a>");
+var enlaceF=$("<a href='#'>Link del final</a>");
+
+$(".contenido").find("p").before(enlaceI);
+$(".contenido").find("p").after(enlaceF);
+
+});
