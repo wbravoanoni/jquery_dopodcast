d70b177824cd3868ad9b0f7bdcd31f4f053f6aab
wbravoanoni <wbravoanoni@gmail.com>
2018-02-08 22:34:15 -0300

    prevenir comportamiento por defecto con jQuery

---
+++ script/script22.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	console.log("DOM cargado script22");
+
+
+
+$("#formularios").on("submit",function(event){
+
+event.preventDefault();
+
+console.log("Formulario enviado");
+
+});
+
+});
+
