58fc0255e02794e77f8dba4d2389cf157621dc7c
wbravoanoni <wbravoanoni@gmail.com>
2018-02-18 18:07:08 -0300

    Eliminar event listeners con jQuery

---
+++ 36_eliminar_event_listeners_con_jquery/js/script36.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	
+console.log("******* script35.js cargado *******");
+
+
+$(".adoptar").children("input").on("click",function(event){
+	event.preventDefault();
+	console.log("boton presionado");
+
+//Con promesas
+	$.getJSON("formulario.php")
+	//.then(respuesta => console.log(respuesta))
+	.then(mostrarCandidatos)
+	.fail(error => console.error(Error(error)))
+	.always(() => console.info('Request finalizado'));
+
+//componer las fichas de los candidatos
+//e insertarlas en el DOM
+
+function mostrarCandidatos(candidatos){
+//$(".candidatos").html("");
+
+console.log(candidatos);
+$.each(candidatos, function(indice,candidato){
+
+console.log(indice+" - "+candidato.nombre);
+
+
+$(`<div class="img">
+	<h4>${candidato.nombre}<i>(${candidato.edad})</i></h4>
+	<h5>${candidato.info}</h5>
+	</div>`)
+.css("background-image","url("+candidato.imagen+")")
+.appendTo($(".candidatos"));
+
+
+});
+
+//$("#boton").attr("disabled",true);
+
+$("#boton").off("click")
+.text("hola");
+
+}
+
+});
+
+
+
+});
