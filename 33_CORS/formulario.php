961235a531c77e3100e8187b55170e895f0b77f6
wbravoanoni <wbravoanoni@gmail.com>
2018-02-18 11:16:53 -0300

    Qué es CORS y para qué sirve

---
+++ 33_CORS/formulario.php
@@ -0,0 +1,19 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+
+if(isset($_POST["nombre"]) && isset($_POST["mensaje"]) ){
+
+//echo "Nombre:".$_POST["nombre"]." Mensaje".$_POST["mensaje"];
+$fecha=Date("d-m-Y H:i:s");
+
+$datos["nombre"]  = $_POST["nombre"];
+$datos["mensaje"] = $_POST["mensaje"];
+$datos["fecha"]   = $fecha;
+
+echo json_encode($datos);
+
+}else{
+	echo "no se ha enviado información";
+}
+?>
---
+++ 33_CORS/js/script33.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	
+console.log("******* script33.js cargado *******");
+
+var formulario=$("#formulario");
+
+
+formulario.on("submit",function(event){
+event.preventDefault();
+//console.log("submit presionado");
+
+
+$.ajax("formulario.php",{
+	type:"POST",
+	dataType:"json",
+	data:{
+		"nombre":$("#nombre").val(),
+		"mensaje":$("#mensaje").val()
+	}
+}).then(function(respuesta){
+	console.log(respuesta.nombre);
+
+var $elemento=$("<li>"+respuesta.nombre+" - <small>"+respuesta.fecha+"</small><br>"+respuesta.mensaje+"</li>");
+
+$("#lista").append($elemento);
+
+
+});
+
+});
+
+});
